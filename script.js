b4f506b2d4436c00978a758aeddeb58665216a29
NullPenPvP <n@j0.si>
2024-09-30 14:01:11 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/api/ping')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('ping-value').textContent = data.ping;
-        document.getElementById('uptime-value').textContent = data.uptime;
-    });
-});
